80489fa7a9e865b6d0c18ed7855fba24e7508fd7
Rana Ziauddin <rana.ziauddin@cloudelligent.com>
2024-12-20 12:54:24 +0500

    updated the code

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -17,7 +17,7 @@ echo "Updating package index after adding PHP repository..."
 sudo apt-get update -y
 
 # Install PHP 8.2 and required extensions
-echo "Installing PHP 8.2 and extensions..."
+echo "Installing PHP 8.2 and extensions...."
 sudo apt-get install -y php8.2-cli php8.2-fpm php8.2-mbstring php8.2-xml php8.2-bcmath php8.2-curl php8.2-zip php8.2-common php8.2-opcache php8.2-readline php8.2-mysql
 
 
